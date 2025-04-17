e5302302f8fac8794b0e5a04a2d04d82326586fd
olegnezvuk91 <olegnezvuk@gmail.com>
2025-04-17 17:05:38 +0200

    7 hw

---
+++ js/task-5.js
@@ -0,0 +1,17 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function handlerBtn() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  spn.textContent = newColor;
+  spn.style.color = newColor;
+}
+
+const btn = document.querySelector(".change-color");
+const spn = document.querySelector(".color");
+
+btn.addEventListener("click", handlerBtn);
